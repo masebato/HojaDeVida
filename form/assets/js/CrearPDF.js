223e0af2cacd91ead8db2330137052ce38f1fbe7
Danilo9508 <danilomurillo09@hotmail.com>
2019-08-07 15:30:33 -0500

    prueba de imagenes

--- form/assets/js/CrearPDF.js
+++ form/assets/js/CrearPDF.js
@@ -5,9 +5,9 @@
 
 function CrearPDF(){
     var doc = new jsPDF();
-    var imgData1 = '../img/0001.jpg';
+    var imgData1 = 'img/0001.jpg';
     var imgData2 = '../img/0002.jpg';
-    var imgData3 = '../img/0003.jpg';
+    var imgData3 = './img/0003.jpg';
     
     doc.setFontSize(40);
     doc.text(40,20,"prueba");
