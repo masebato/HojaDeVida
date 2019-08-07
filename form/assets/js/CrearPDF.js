5ac0df9342c84cf6f943d74bed43f793e85dadaf
Marx sebastian <baradona69@gmail.com>
2019-08-07 15:47:34 -0500

    Carga funcion subir imagen

--- form/assets/js/CrearPDF.js
+++ form/assets/js/CrearPDF.js
@@ -3,19 +3,51 @@
 
 
 
-function CrearPDF(){
-    var doc = new jsPDF();
-    var imgData1 = '../img/0001.jpg';
-    console.log(imgData1);
-    var imgData2 = '../img/0002.jpg';
-    var imgData3 = '../img/0003.jpg';
-    
-    doc.setFontSize(40);
-    doc.text(40,20,"prueba");
-    doc.addImage(imgData1, 'JPG', 10,40,180,180);
-    console.log(imgData1);
-    
+// Because of security restrictions, getImageFromUrl will
+// not load images from other domains.  Chrome has added
+// security restrictions that prevent it from loading images
+// when running local files.  Run with: chromium --allow-file-access-from-files --allow-file-access
+// to temporarily get around this issue.
+var getImageFromUrl = function(url, callback) {
+    var img = new Image();
+
+    img.onError = function() {
+        alert('Cannot load image: "'+url+'"');
+    };
+    img.onload = function() {
+        callback(img);
+    };
+    img.src = url;
 }
+
+// Since images are loaded asyncronously, we must wait to create
+// the pdf until we actually have the image.
+// If we already had the jpeg image binary data loaded into
+// a string, we create the pdf without delay.
+var createPDF = function(imgData) {
+    var doc = new jsPDF();
+
+    // This is a modified addImage example which requires jsPDF 1.0+
+    // You can check the former one at examples/js/basic.js
+
+    doc.addImage(imgData, 'JPEG', 10, 10, 50, 50, 'img'); // Cache the image using the alias 'monkey'
+    doc.addImage('img', 70, 10, 100, 120); // use the cached 'monkey' image, JPEG is optional regardless
+    // As you can guess, using the cached image reduces the generated PDF size by 50%!
+
+    // Rotate Image - new feature as of 2014-09-20
+    doc.addImage({
+        imageData : imgData,
+        angle     : -20,
+        x         : 10,
+        y         : 78,
+        w         : 45,
+        h         : 58
+    });
+
+    // Output as Data URI
+    doc.output('datauri');
+}
+
 
 
 //#endregion
