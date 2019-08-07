633c8fe559e02fd5dde7fa4b993159835976a402
Marx sebastian <baradona69@gmail.com>
2019-08-07 15:29:06 -0500

    cambios imagenes

--- form/assets/js/CrearPDF.js
+++ form/assets/js/CrearPDF.js
@@ -6,13 +6,14 @@
 function CrearPDF(){
     var doc = new jsPDF();
     var imgData1 = '../img/0001.jpg';
+    console.log(imgData1);
     var imgData2 = '../img/0002.jpg';
     var imgData3 = '../img/0003.jpg';
     
     doc.setFontSize(40);
     doc.text(40,20,"prueba");
     doc.addImage(imgData1, 'JPG', 10,40,180,180);
-    
+    console.log(imgData1);
     
 }
 
